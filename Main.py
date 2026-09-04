0607a418957c5bca8d623b1b7f3af3dce0ce0864
Imanuel Ekselo Dali <ATsniper315@gmail.com>
2026-09-04 21:12:58 +0700

    Tambah Validasi Uang Kurang

--- Main.py
+++ Main.py
@@ -4,14 +4,20 @@ harga = int(input("Harga barang: "))
 jumlah = int(input("Jumlah beli: "))
 
 total = harga * jumlah
+
+# Mengedit bagian ini untuk potongan harga 
+if total > 50000:
+    print("Dapat potongan 5000 karena belanja di atas 50rb!")
+    total = total - 5000
+
 print("Total bayar:", total)
 
 bayar = int(input("Uang cash: "))
 
-# Mengedit bagian ini agar bisa cek uang kurang
 if bayar >= total:
     kembalian = bayar - total
     print("Kembalian anda:", kembalian)
+    print("Terima kasih sudah berbelanja!")
 else:
     kurang = total - bayar
     print("Uang anda kurang sebesar:", kurang)
